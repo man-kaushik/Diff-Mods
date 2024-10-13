083d14888b39937fccd5c013981f5c70bea092f9
Mannu <ms4055028@gmail.com>
2024-10-13 20:42:41 +0530

    Adding Semantic UI and RouterLink to Nav Bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ElementsHomeComponent } from './elements/elements-home/elements-home.component';
 import { CollectionsHomeComponent } from "./collections/collections-home/collections-home.component";
 import { ModsHomeComponent } from "./mods/mods-home/mods-home.component";
@@ -8,7 +8,7 @@ import { ViewsHomeComponent } from "./views/views-home/views-home.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ElementsHomeComponent, CollectionsHomeComponent, ModsHomeComponent, ViewsHomeComponent],
+  imports: [RouterOutlet, ElementsHomeComponent, CollectionsHomeComponent, ModsHomeComponent, ViewsHomeComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { ElementsHomeComponent } from './elements/elements-home/elements-home.co
 import { CollectionsHomeComponent } from './collections/collections-home/collections-home.component';
 import { ModsHomeComponent } from './mods/mods-home/mods-home.component';
 import { ViewsHomeComponent } from './views/views-home/views-home.component';
+import { ElementsModule } from './elements/elements.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
+    {path: '', component: HomeComponent},
     {path: 'elements', component: ElementsHomeComponent},
     {path: 'collections', component: CollectionsHomeComponent},
     {path: 'mods', component: ModsHomeComponent},
-    {path: 'views', component: ViewsHomeComponent}
+    {path: 'views', component: ViewsHomeComponent},
+    {path: '**', component: NotFoundComponent}
 ];
