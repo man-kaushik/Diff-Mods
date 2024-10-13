8ef82275c67d7f30d1578e76c5f7399db2a8fb3c
Mannu <ms4055028@gmail.com>
2024-10-13 20:01:37 +0530

    Added basic Routing for all 4 components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ElementsHomeComponent } from './elements/elements-home/elements-home.component';
+import { CollectionsHomeComponent } from './collections/collections-home/collections-home.component';
+import { ModsHomeComponent } from './mods/mods-home/mods-home.component';
+import { ViewsHomeComponent } from './views/views-home/views-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'elements', component: ElementsHomeComponent},
+    {path: 'collections', component: CollectionsHomeComponent},
+    {path: 'mods', component: ModsHomeComponent},
+    {path: 'views', component: ViewsHomeComponent}
+];
