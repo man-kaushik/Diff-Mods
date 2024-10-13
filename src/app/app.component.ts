4d47bf93b72c00bfc7e582f33ffef9b60068197a
Mannu <ms4055028@gmail.com>
2024-10-13 20:51:38 +0530

    Added Content inside Home and NotFound Template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,13 @@ import { ElementsHomeComponent } from './elements/elements-home/elements-home.co
 import { CollectionsHomeComponent } from "./collections/collections-home/collections-home.component";
 import { ModsHomeComponent } from "./mods/mods-home/mods-home.component";
 import { ViewsHomeComponent } from "./views/views-home/views-home.component";
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ElementsHomeComponent, CollectionsHomeComponent, ModsHomeComponent, ViewsHomeComponent, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, ElementsHomeComponent, CollectionsHomeComponent, ModsHomeComponent, ViewsHomeComponent, RouterLink, RouterLinkActive,HomeComponent,NotFoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
