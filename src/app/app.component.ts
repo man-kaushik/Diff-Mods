7af26d120515fbc08ad5da10a8f6a023b37392eb
Mannu <ms4055028@gmail.com>
2024-10-14 21:44:51 +0530

    Lazy Loading and removing import from app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { ElementsHomeComponent } from './elements/elements-home/elements-home.component';
-import { CollectionsHomeComponent } from "./collections/collections-home/collections-home.component";
-import { ModsHomeComponent } from "./mods/mods-home/mods-home.component";
-import { ViewsHomeComponent } from "./views/views-home/views-home.component";
+
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ElementsHomeComponent, CollectionsHomeComponent, ModsHomeComponent, ViewsHomeComponent, RouterLink, RouterLinkActive,HomeComponent,NotFoundComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive,HomeComponent,NotFoundComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,9 +9,22 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'elements', component: ElementsHomeComponent},
-    {path: 'collections', component: CollectionsHomeComponent},
-    {path: 'mods', component: ModsHomeComponent},
-    {path: 'views', component: ViewsHomeComponent},
+    //Lazy Loading
+    {
+        path: 'elements',
+        loadChildren: () => import('./elements/elements.module').then(m => m.ElementsModule)
+    },
+    {
+        path: 'collections',
+        loadChildren:() => import('./collections/collections.module').then(m => m.CollectionsModule)
+    },
+    {
+        path: 'mods',
+        loadChildren:() => import('./mods/mods.module').then(m => m.ModsModule)
+    },
+    {
+        path: 'views',
+        loadChildren:() => import('./views/views.module').then(m => m.ViewsModule)
+    },
     {path: '**', component: NotFoundComponent}
 ];
