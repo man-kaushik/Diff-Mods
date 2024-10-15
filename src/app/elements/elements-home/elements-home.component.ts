19135291ce635d4b8737ea1bd4aedb21c8dac6de
Mannu <ms4055028@gmail.com>
2024-10-15 23:43:06 +0530

    Adding Divider Component in Shared Module as it is going to be used by all the components

--- src/app/elements/elements-home/elements-home.component.ts
+++ src/app/elements/elements-home/elements-home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PlaceholderComponent } from "../placeholder/placeholder.component";
+import { DividerComponent } from "../../shared/divider/divider.component";
 
 @Component({
   selector: 'app-elements-home',
   standalone: true,
-  imports: [PlaceholderComponent],
+  imports: [PlaceholderComponent, DividerComponent],
   templateUrl: './elements-home.component.html',
   styleUrl: './elements-home.component.css'
 })
