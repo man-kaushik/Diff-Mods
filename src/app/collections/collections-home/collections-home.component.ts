f3f4c8afd2ad48a8952233f53f3324cd07e53a52
Mannu <ms4055028@gmail.com>
2024-10-16 00:34:43 +0530

    Added Some Static data and also generated Table component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { TableComponent } from '../table/table.component';
+import { DividerComponent } from '../../shared/divider/divider.component';
 
 @Component({
   selector: 'app-collections-home',
   standalone: true,
-  imports: [],
+  imports: [TableComponent,DividerComponent],
   templateUrl: './collections-home.component.html',
   styleUrl: './collections-home.component.css'
 })
 export class CollectionsHomeComponent {
+  data = [
+    { name:'James', age: 24, job: 'Designer'},
+    { name:'Jill', age: 26, job: 'Engineer'},
+    { name:'Elyse', age: 25, job: 'Engineer'}
+  ];
+  headers = [
+    {key: 'name',label: 'Name'},
+    {key: 'age',label: 'Age'},
+    {key: 'job',label: 'Job'}
+  ];
 
 }
---
+++ src/app/collections/table/table.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './table.component.html',
+  styleUrl: './table.component.css'
+})
+export class TableComponent {
+ @Input() data:any = [];
+ @Input() headers:any = [];
+}
