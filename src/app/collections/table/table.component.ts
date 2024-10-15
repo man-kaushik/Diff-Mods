eb354f8f2c3d6bda26825acbd6c2acfcca7d1a16
Mannu <ms4055028@gmail.com>
2024-10-16 00:44:54 +0530

    Adding table and Static data relating column and values through key

--- src/app/collections/table/table.component.ts
+++ src/app/collections/table/table.component.ts
@@ -1,13 +1,15 @@
+import { NgFor } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css'
 })
 export class TableComponent {
+  @Input() classNames='';
  @Input() data:any = [];
  @Input() headers:any = [];
 }
