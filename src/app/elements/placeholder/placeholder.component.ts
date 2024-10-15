346d6ffbd8d2071474a50bbeeeb6e05ff63c8d65
Mannu <ms4055028@gmail.com>
2024-10-15 23:32:28 +0530

    Made TImes Directive and edit Placeholder Component

--- src/app/elements/placeholder/placeholder.component.ts
+++ src/app/elements/placeholder/placeholder.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { TimesDirective } from '../times.directive';
 
 @Component({
   selector: 'app-placeholder',
   standalone: true,
-  imports: [],
+  imports: [NgIf,TimesDirective],
   templateUrl: './placeholder.component.html',
   styleUrl: './placeholder.component.css'
 })
 export class PlaceholderComponent {
+  //Declared with Default value
+  @Input() header=true;
+  @Input() lines=3;
 
 }
