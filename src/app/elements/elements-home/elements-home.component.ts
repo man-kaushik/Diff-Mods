95e4708d6139e77e22b311926661d1bb90cba4a7
Mannu <ms4055028@gmail.com>
2024-10-16 00:01:08 +0530

    Added Segment component and implementing that in ElementHome with ng-content select property

--- src/app/elements/elements-home/elements-home.component.ts
+++ src/app/elements/elements-home/elements-home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { PlaceholderComponent } from "../placeholder/placeholder.component";
 import { DividerComponent } from "../../shared/divider/divider.component";
+import { SegmentComponent } from "../segment/segment.component";
 
 @Component({
   selector: 'app-elements-home',
   standalone: true,
-  imports: [PlaceholderComponent, DividerComponent],
+  imports: [PlaceholderComponent, DividerComponent, SegmentComponent],
   templateUrl: './elements-home.component.html',
   styleUrl: './elements-home.component.css'
 })
