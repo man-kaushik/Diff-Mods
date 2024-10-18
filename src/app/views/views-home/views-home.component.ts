cea7df0f0722dd8327e696d0655eb5d62f396326
Mannu <ms4055028@gmail.com>
2024-10-19 02:59:01 +0530

    Worked on Views Component to build Statistics and Item List

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { StatisticsComponent } from "../statistics/statistics.component";
+import { DividerComponent } from '../../shared/divider/divider.component';
+import { ItemListComponent } from "../item-list/item-list.component";
 
 @Component({
   selector: 'app-views-home',
   standalone: true,
-  imports: [],
+  imports: [StatisticsComponent, DividerComponent, ItemListComponent],
   templateUrl: './views-home.component.html',
   styleUrl: './views-home.component.css'
 })
 export class ViewsHomeComponent {
+
+  stats :any  = [
+    {value:22, label: '# of Users'},
+    {value:900, label:'Revenue'},
+    {value:50, label:'Reviews'}
+  ];
+  items :any = [
+    { image: 'couch.jpeg',
+      title:'Couch',
+      description: "This is a fantastic couch to sit on"
+    },
+    {
+      image: 'dresser.jpeg',
+      title:'Dresser',
+      description: "This is a great dresser to put stuff in"
+    }
+  ];
 
 }
