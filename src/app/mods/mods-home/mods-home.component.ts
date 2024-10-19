424f4817a0e7922da5d6154a59f3ceef922894dc
Mannu <ms4055028@gmail.com>
2024-10-20 03:56:25 +0530

    Modal Component Added

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { ModalComponent } from "../modal/modal.component";
+import { SharedModule } from '../../shared/shared.module';
+import { DividerComponent } from '../../shared/divider/divider.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-mods-home',
   standalone: true,
-  imports: [],
+  imports: [ModalComponent,DividerComponent,NgIf],
   templateUrl: './mods-home.component.html',
   styleUrl: './mods-home.component.css'
 })
 export class ModsHomeComponent {
+  modalOpen=false;
 
+  onClick(){
+    this.modalOpen=!this.modalOpen;
+  }
 }
