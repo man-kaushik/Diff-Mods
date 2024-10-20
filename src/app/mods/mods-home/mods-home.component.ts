204cf038843ecd6098a96dbc881614f70b3cf4d2
Mannu <ms4055028@gmail.com>
2024-10-20 13:48:26 +0530

    Added Accordion Modal to last page

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -3,16 +3,23 @@ import { ModalComponent } from "../modal/modal.component";
 import { SharedModule } from '../../shared/shared.module';
 import { DividerComponent } from '../../shared/divider/divider.component';
 import { NgIf } from '@angular/common';
+import { AccordionComponent } from '../accordion/accordion.component';
+import { title } from 'process';
 
 @Component({
   selector: 'app-mods-home',
   standalone: true,
-  imports: [ModalComponent,DividerComponent,NgIf],
+  imports: [ModalComponent,DividerComponent,NgIf,AccordionComponent],
   templateUrl: './mods-home.component.html',
   styleUrl: './mods-home.component.css'
 })
 export class ModsHomeComponent {
   modalOpen=false;
+  items:any = [
+    {title:'Why is the sky blue?', content:'It is what it is.'},
+    {title:'What does an orange taste like?', content:'Its tangy, sour and what not.'},
+    {title:'What color is the sky?', content:'Orange, I can say.'}
+  ]
 
   onClick(){
     this.modalOpen=!this.modalOpen;
